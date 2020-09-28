1c18e5b32e72383e5fe0ba978975121b302a400f
Mr. G <jorge8486@gmail.com>
2020-09-27 21:54:17 -0400

    added tweet form and sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'twitter-clone';
+  onSubmitTweet(form: NgForm) {
+    alert(form.value.tweet);
+  }
 }
