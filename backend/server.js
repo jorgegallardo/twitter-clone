b77253e4b36bb1151bc2d3db7ae6bbac318d7196
Mr. G <jorge8486@gmail.com>
2020-09-28 16:51:24 -0400

    hard-coded tweets on the server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  res.json({ message: "hi there" });
+app.get("/tweets", (req, res) => {
+  const tweets = [
+    { id: "asdfh2323", message: "hi there" },
+    { id: "faslje372", message: "bye there" },
+  ];
+  res.status(200).json({ message: "Fetched tweets successfully.", tweets });
 });
 
 app.listen(3000, () => {
